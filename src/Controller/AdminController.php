5de040fcbee217c73480ba903db9ad950f97e7a6
Adrien KOUYOUMJIAN <adrien.kouyoumjian@outlook.fr>
2023-06-13 12:28:16 +0200

    gestion des articles

--- src/Controller/AdminController.php
+++ src/Controller/AdminController.php
@@ -2,6 +2,9 @@
 
 namespace App\Controller;
 
+use App\Entity\Article;
+use App\Repository\ArticleRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -12,5 +15,19 @@ class AdminController extends AbstractController
     public function index(): Response
     {
         return $this->render('admin/index.html.twig');
+    }
+
+    #[Route('/admin/article', name: 'admin_article')]
+    public function adminArticle(ArticleRepository $repo, EntityManagerInterface $manager)
+    {
+        $colonnes = $manager->getClassMetadata(Article::class)->getFieldNames();
+
+        // dd($colonnes);
+        $articles = $repo->findAll();
+
+        return $this->render('admin/gestionArticle.html.twig', [
+            "colonnes" => $colonnes,
+            'articles' => $articles
+        ]);
     }
 }
